3b9316a9f697ba2ef7cf18823202d3a5daa729f9
jessicalpires <jessicapireslx@gmail.com>
2020-07-26 18:28:12 +0100

    Update time

--- src/script.js
+++ src/script.js
@@ -1,12 +1,23 @@
 function formatDate(timestamp){
-    let now = new Date(timestamp);
-    let days = ["SUN", "MON", "TUE", "WED","THU", "FRI", "SAT"];
-    let currentDay = days[now.getDay()];
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let currentMonth = months[now.getMonth()];
-    let currentDate = now.getDate();
-    let currentHour = now.getHours();
-    let currentMinutes = now.getMinutes();
+    let date = newDate(timestamp);
+    let hours = date.getHours();
+    if (hours < 10){
+        hours=`0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10){
+        minutes=`0${minutes}`;
+    }
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Saturday"
+    ];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
 }
 
 function showTemperature(response) {
