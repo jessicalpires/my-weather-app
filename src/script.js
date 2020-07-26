91005e4de124b889d29cff3d8ec8a5cc57ee0e02
jessicalpires <jessicapireslx@gmail.com>
2020-07-26 19:04:32 +0100

    Added the relevant icon from API

--- src/script.js
+++ src/script.js
@@ -21,23 +21,35 @@ function formatDate(timestamp){
 }
 
 function showTemperature(response) {
-    let temperatureElement = document.querySelector("current-temperature");
+    let temperatureElement = document.querySelector("#current-temperature");
     temperatureElement.innerHTML=Math.round(response.data.main.temp);
-    let cityElement = document.querySelector("city");
+    let cityElement = document.querySelector("#city");
     cityElement.innerHTML = response.data.name;
-    let descriptionElement = document.querySelector("current-weather-description");
+    let descriptionElement = document.querySelector("#current-weather-description");
     descriptionElement.innerHTML = response.data.weather[0].description;
-    let humidityElement = document.querySelector("humidity");
+    let humidityElement = document.querySelector("#humidity");
     humidityElement.innerHTML = response.data.main.humidity;
     let windElement = document.querySelector("#wind");
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    let iconElement = document.querySelector("#icon");
+    iconElement.innerHTML=`http://openweathermap.org/img/wn/01d@2x.png`
 }
+
+iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+);
+
+iconElement.setAttribute(
+    "alt",
+    response.data.weather[0].description
+);
 
 
 function handleSubmit(event){
     event.preventDefault();
     let apiKey = "9867db028fe476d7756323ae2169ee2f";
-    let city = document.querySelector("#type-city").value;
+    let city = "New York";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(showTemperature)}
 
